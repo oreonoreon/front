6a810b250b18351cc3b55008a9320f88f47b12cb
Yaroslav Prokhorov <y.prokhorov@gardatech.ru>
2025-11-08 16:13:45 +0300

    попробуем ркшить проблему с невозможглстью залогироваться на некоторых клиентах

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
+        rewrite: path => path.replace(/^\/api/, ''),
+        // Для корректной работы с cookies
+        configure: (proxy, options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            // Логирование для отладки
+            console.log('Proxying:', req.method, req.url);
+          });
+        }
       }
     }
   }
